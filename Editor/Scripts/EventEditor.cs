47ad11bf12f0f657d7a65a7005bdda38f2e5f872
Lee, Hyeon-gi <maaya.lee@gmail.com>
2022-05-11 19:54:22 +0900

    테마2 스프라이트 교체

---
+++ Editor/Scripts/EventEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Subsets.Message2
+{
+    [CustomEditor(typeof(GameEvent), editorForChildClasses: true)]
+    public class EventEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            GameEvent e = target as GameEvent;
+            if (GUILayout.Button("Raise"))
+                e.Raise();
+        }
+    }
+}

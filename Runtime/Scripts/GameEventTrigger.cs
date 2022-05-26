0e777559d75ccea9775fc97433561b93e788d457
Lee, Hyeon-gi <maayalee@gmail.com>
2022-05-27 03:35:32 +0900

    씬정리, 스킵 기능

---
+++ Runtime/Scripts/GameEventTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Subsets.Message2
+{
+    public class GameEventTrigger : MonoBehaviour
+    {
+        public GameEvent RaiseEvent;
+        public Camera Camera;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Transform objectHit = hit.transform;
+                    Debug.Log("GameEventTrigger" + objectHit.name);
+                    if (RaiseEvent)
+                    {
+                        RaiseEvent.Raise();
+                    }
+                }
+            }
+
+        }
+    }
+}
